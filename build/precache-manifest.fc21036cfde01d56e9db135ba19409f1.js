afa50f4ce30e82172320823333579240fb0d2199
Bohdan Yelmanov <yelmanovbohdan@gmail.com>
2021-03-29 23:06:49 +0300

    add build

---
+++ build/precache-manifest.fc21036cfde01d56e9db135ba19409f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92903588db85848db64b24af46780f6",
+    "url": "/truthOrDareReact/index.html"
+  },
+  {
+    "revision": "227fedd57b34bdeb1846",
+    "url": "/truthOrDareReact/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "96daa655f36dec9868f7",
+    "url": "/truthOrDareReact/static/css/main.b658092d.chunk.css"
+  },
+  {
+    "revision": "227fedd57b34bdeb1846",
+    "url": "/truthOrDareReact/static/js/2.1fc5b171.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/truthOrDareReact/static/js/2.1fc5b171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96daa655f36dec9868f7",
+    "url": "/truthOrDareReact/static/js/main.6325a6ef.chunk.js"
+  },
+  {
+    "revision": "3d023c55ee52b844d6b6",
+    "url": "/truthOrDareReact/static/js/runtime-main.7eb7695c.js"
+  },
+  {
+    "revision": "4fedae32dc5fd5f2acabbeb85f141e1c",
+    "url": "/truthOrDareReact/static/media/logo-main.4fedae32.png"
+  },
+  {
+    "revision": "14842a697b8491e9684c147ba5655110",
+    "url": "/truthOrDareReact/static/media/wine-bottle.14842a69.png"
+  }
+]);
