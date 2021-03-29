edab46bba57dc8b629cdf3620ee6b5835494a79a
Bohdan Yelmanov <yelmanovbohdan@gmail.com>
2021-03-29 21:14:14 +0300

    added build

---
+++ build/precache-manifest.9604573b5d210d141f68908e688662bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b916333069884cc22b2b40ad1724de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "227fedd57b34bdeb1846",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "37ea1ff528e0572bab21",
+    "url": "/static/css/main.97432745.chunk.css"
+  },
+  {
+    "revision": "227fedd57b34bdeb1846",
+    "url": "/static/js/2.1fc5b171.chunk.js"
+  },
+  {
+    "revision": "af1b71dc8f7cc14f3b4c1f4697374759",
+    "url": "/static/js/2.1fc5b171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ea1ff528e0572bab21",
+    "url": "/static/js/main.18bb8ec7.chunk.js"
+  },
+  {
+    "revision": "dd1e94ac446f090b68ab",
+    "url": "/static/js/runtime-main.5dfc168c.js"
+  },
+  {
+    "revision": "4fedae32dc5fd5f2acabbeb85f141e1c",
+    "url": "/static/media/logo-main.4fedae32.png"
+  },
+  {
+    "revision": "14842a697b8491e9684c147ba5655110",
+    "url": "/static/media/wine-bottle.14842a69.png"
+  }
+]);
